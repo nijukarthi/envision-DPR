9c9be72ddb94a7de9751ab24bc5c45d19263c7a5
karthikeyan <karthikeyan@kalajitha.com>
2025-09-23 11:37:20 +0530

    UI Changes

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -71,7 +71,31 @@ export class AppMenu {
                             {
                                 label: 'Production Manager',
                                 icon: 'pi pi-fw pi-box',
-                                routerLink: ['/app/uikit/productionManager']
+                                routerLink: ['/app/uikit/productionManager'],
+                                 items: [
+                            {
+                                label: 'Nacelle & Hub',
+                                icon: 'pi pi-fw pi-bookmark',
+                               /*  items: [
+                                    { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+                                    { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+                                ] */
+                            },
+                            {
+                                label: 'Blade',
+                                icon: 'pi pi-fw pi-bookmark',
+                               /*  items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }] */
+                            },
+                            {
+                                label: 'Tower',
+                                icon: 'pi pi-fw pi-bookmark',
+                               /*  items: [
+                                    { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+                                    { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+                                ] */
+                            },
+                            
+                        ],
                             },
                             {
                                 label: 'Delivery Manager',
